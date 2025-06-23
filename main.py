95f029265bf00dcb9892b0dada10632e5d725cbb
Waterbootdev <wassermann.sp@gmail.com>
2025-06-23 14:22:25 +0200

    Refactor main function to utilize init for setup; remove redundant code and improve clarity

--- main.py
+++ main.py
@@ -21,67 +21,32 @@ DRIVE = "/mnt/g"
 
 FIRST_LINE = escape_position(3,1)
 SECOND_LINE = escape_position(5,1)
-EXTRA_LINE = escape_position(8,1)
-    
+
+def init(width: int, height: int):
+    random: RandomPieceGenerator = RandomPieceGenerator(width, height, firts_frame_piece_keys=[PIECE_KEYS[0]], piece_keys=[p for p in PIECE_KEYS if '0' not in p])
+    counts: PieceKeyCounts  = PieceKeyCounts(piece_key_groups_counts_sum(random.pieces))
+    first_frame: List[str]  = [p.inital_piece_key for p in random.spiral if p.frame_index == 0]
+    index_pool: IndexPool = IndexPool(1)
+    node_counter: NodeCounter = NodeCounter()
+    generated_pieces: PieceKeyCountsPieceGenerator = PieceKeyCountsPieceGenerator(width, height, index_pool, node_counter, first_frame, counts.asterisk_piece_key_counts)
+    pieces : List[PieceKeyCountsPiece] = generated_pieces.pieces
+    return counts,index_pool,node_counter,pieces
+
 def main():
 
     setrlimit(RLIMIT_NOFILE, (1000000, 1000000))
-
     system('clear')
-
     current_argv = sys.argv
-   
     width, height, subdirctory = get_from_argvs(current_argv)
-
     directory_path_name = path.join(DRIVE, subdirctory)
-
     directory_path: Path = Path(directory_path_name)
-
     if not directory_path.exists():
         directory_path.mkdir()
-    
-    random: RandomPieceGenerator = RandomPieceGenerator(width, height, firts_frame_piece_keys=[PIECE_KEYS[0]], piece_keys=[p for p in PIECE_KEYS if '0' not in p])
-
-    counts: PieceKeyCounts  = PieceKeyCounts(piece_key_groups_counts_sum(random.pieces))
-    first_frame: List[str]  = [p.inital_piece_key for p in random.spiral if p.frame_index == 0]
-
-    index_pool: IndexPool = IndexPool(1)
-    node_counter: NodeCounter = NodeCounter()
-
-    generated_pieces: PieceKeyCountsPieceGenerator = PieceKeyCountsPieceGenerator(width, height, index_pool, node_counter, first_frame, counts.asterisk_piece_key_counts)
-
-    pieces : List[PieceKeyCountsPiece] = generated_pieces.pieces
-
+    counts, index_pool, node_counter, pieces = init(width, height)
     start1 = time.time()
-
     save_solutions(directory_path_name, index_pool, node_counter, counts, pieces)
-    
     end1 = time.time()
-
-    print(EXTRA_LINE)
+    print(SECOND_LINE)
     print(f'{end1 - start1}')
-
-    # counts: PieceKeyCounts  = PieceKeyCounts(piece_key_groups_counts_sum(random.pieces))
-    
-    # extra_generated_pieces: ExtraPieceKeyCountsPieceGenerator = ExtraPieceKeyCountsPieceGenerator(width, height, first_frame, counts.asterisk_piece_key_counts)
-
-    # extra_pieces : List[ExtraPieceKeyCountsPiece] = extra_generated_pieces.pieces
-
-    # directory_path_name = path.join(DRIVE, f'{subdirctory}_extra')
-
-    # directory_path: Path = Path(directory_path_name)
-
-    # if not directory_path.exists():
-    #     directory_path.mkdir()
-
-    # start2 = time.time()
-
-    # save_solutions(directory_path_name, counts, extra_pieces)
-
-    # end2 = time.time()
-
-
-    # print(EXTRA_LINE)
-    # print(f'{end1 - start1}:{end2 - start2}')
 
 main()
